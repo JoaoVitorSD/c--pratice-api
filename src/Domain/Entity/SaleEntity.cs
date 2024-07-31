3978f01a3ddd360689005ed15d8e0c405c4e4a55
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2024-07-31 10:16:22 -0300

    merge with main

--- src/Domain/Entity/SaleEntity.cs
+++ src/Domain/Entity/SaleEntity.cs
@@ -1,7 +1,7 @@
 public class Sale
 {
     public int Id { get; set; }
-    public string ProductName { get; set; }
+    public required string ProductName { get; set; }
     public decimal Price { get; set; }
     public DateTime SaleDate { get; set; }
 }
